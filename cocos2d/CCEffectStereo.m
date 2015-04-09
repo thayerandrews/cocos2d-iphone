f1b080c54ad33c097914ba33c53b10c23b14f633
Thayer J Andrews <thayer@apportable.com>
2015-04-09 09:55:31 -0600

    CCEffectStereo - Initial Metal implementation
    Because of problems with destination color reads, this isn't functional yet. It compiles, builds, and runs
    without error but it just draws white as a workaround for the destination read issue.

--- cocos2d/CCEffectStereo.m
+++ cocos2d/CCEffectStereo.m
@@ -12,18 +12,18 @@
 
 #import "CCEffectShader.h"
 #import "CCEffectShaderBuilderGL.h"
+#import "CCEffectShaderBuilderMetal.h"
 #import "CCEffect_Private.h"
 #import "CCRenderer.h"
+#import "CCSetup.h"
 #import "CCTexture.h"
 
 
+#pragma mark - CCEffectSteroImplGL
 
 @interface CCEffectStereoImplGL : CCEffectImpl
-
 @property (nonatomic, weak) CCEffectStereo *interface;
-
 @end
-
 
 @implementation CCEffectStereoImplGL
 
@@ -122,6 +122,117 @@
 @end
 
 
+#pragma mark - CCEffectStereoImplMetal
+
+@interface CCEffectStereoImplMetal : CCEffectImpl
+@property (nonatomic, weak) CCEffectStereo *interface;
+@end
+
+
+@implementation CCEffectStereoImplMetal
+
+-(id)initWithInterface:(CCEffectStereo *)interface
+{
+    NSArray *renderPasses = [CCEffectStereoImplMetal buildRenderPassesWithInterface:interface];
+    NSArray *shaders = [CCEffectStereoImplMetal buildShaders];
+    
+    if((self = [super initWithRenderPasses:renderPasses shaders:shaders]))
+    {
+        self.interface = interface;
+        self.debugName = @"CCEffectStereoImplMetal";
+    }
+    return self;
+}
+
++ (NSArray *)buildShaders
+{
+    return @[[[CCEffectShader alloc] initWithVertexShaderBuilder:[CCEffectShaderBuilderMetal defaultVertexShaderBuilder] fragmentShaderBuilder:[CCEffectStereoImplMetal fragShaderBuilder]]];
+}
+
++ (CCEffectShaderBuilder *)fragShaderBuilder
+{
+    NSArray *functions = [CCEffectStereoImplMetal buildFragmentFunctions];
+    NSArray *temporaries = @[
+                             [CCEffectFunctionTemporary temporaryWithType:@"half4" name:@"tmp" initializer:CCEffectInitPreviousPass],
+                             [CCEffectFunctionTemporary temporaryWithType:@"half4" name:@"dstColor" initializer:CCEffectInitFragColor]
+                             ];
+    NSArray *calls = @[[[CCEffectFunctionCall alloc] initWithFunction:functions[0] outputName:@"stereoSeparated" inputs:@{@"channelSelect" : @"channelSelect",
+                                                                                                                          @"dstColor" : @"dstColor",
+                                                                                                                          @"inputValue" : @"tmp"}]];
+    NSArray *arguments = @[
+                           [[CCEffectShaderArgument alloc] initWithType:@"const device float&" name:@"channelSelect" qualifier:CCEffectShaderArgumentBuffer],
+
+// XXX The following should work but currently results in a Metal pipeline state that won't compile. My
+// guess is that the associated state for color attachment 0 needs to be adjusted somehow to support
+// shader reads from it. Since this is an experimental effect anyway, I'm leaving the investigation until
+// after I've implemented the remainder of the non-experimental effects.
+//
+//                           [[CCEffectShaderArgument alloc] initWithType:@"half4" name:@"dstColor" qualifier:CCEffectShaderArgumentDstColor]
+                           ];
+    
+    return [[CCEffectShaderBuilderMetal alloc] initWithType:CCEffectShaderBuilderFragment
+                                                  functions:functions
+                                                      calls:calls
+                                                temporaries:temporaries
+                                                  arguments:[[CCEffectShaderBuilderMetal defaultFragmentArguments] arrayByAddingObjectsFromArray:arguments]
+                                                    structs:[CCEffectShaderBuilderMetal defaultStructDeclarations]];
+}
+
++ (NSArray *)buildFragmentFunctions
+{
+    NSString* effectBody = CC_GLSL(
+                                   half4 result;
+                                   float dstAlpha = 1.0 - inputValue.a;
+
+                                   if (channelSelect == 0.0)
+                                   {
+                                       result = half4(inputValue.r + dstColor.r * dstAlpha, dstColor.g, dstColor.b, 1);
+                                   }
+                                   else
+                                   {
+                                       result = half4(dstColor.r, inputValue.g + dstColor.g * dstAlpha, inputValue.b + dstColor.b * dstAlpha, 1);
+                                   }
+                                   return result;
+                                   );
+    
+    NSArray *inputs = @[
+                        [[CCEffectFunctionInput alloc] initWithType:@"half4" name:@"inputValue"],
+                        [[CCEffectFunctionInput alloc] initWithType:@"half4" name:@"dstColor"],
+                        [[CCEffectFunctionInput alloc] initWithType:@"const device float&" name:@"channelSelect"],
+                        ];
+    return @[[[CCEffectFunction alloc] initWithName:@"stereoEffect" body:effectBody inputs:inputs returnType:@"half4"]];
+}
+
++ (NSArray *)buildRenderPassesWithInterface:(CCEffectStereo *)interface
+{
+    __weak CCEffectStereo *weakInterface = interface;
+    
+    CCEffectRenderPass *pass0 = [[CCEffectRenderPass alloc] init];
+    pass0.debugLabel = @"CCEffectStereo pass 0";
+    pass0.beginBlocks = @[[[CCEffectRenderPassBeginBlockContext alloc] initWithBlock:^(CCEffectRenderPass *pass, CCEffectRenderPassInputs *passInputs){
+        
+        passInputs.shaderUniforms[CCShaderUniformMainTexture] = passInputs.previousPassTexture;
+        passInputs.shaderUniforms[CCShaderUniformPreviousPassTexture] = passInputs.previousPassTexture;
+        
+        CCEffectTexCoordDimensions tcDims;
+        tcDims.texCoord1Center = passInputs.texCoord1Center;
+        tcDims.texCoord1Extents = passInputs.texCoord1Extents;
+        tcDims.texCoord2Center = passInputs.texCoord2Center;
+        tcDims.texCoord2Extents = passInputs.texCoord2Extents;
+        passInputs.shaderUniforms[CCShaderArgumentTexCoordDimensions] = [NSValue valueWithBytes:&tcDims objCType:@encode(CCEffectTexCoordDimensions)];
+        
+        passInputs.shaderUniforms[passInputs.uniformTranslationTable[@"channelSelect"]] = (interface.channelSelect == CCEffectStereoSelectRed) ? @(0.0f) : @(1.0f);
+        
+    }]];
+    
+    return @[pass0];
+}
+
+@end
+
+
+#pragma mark - CCEffectStero
+
 @implementation CCEffectStereo
 
 -(id)init
@@ -135,7 +246,15 @@
     {
         _channelSelect = channelSelect;
         
-        self.effectImpl = [[CCEffectStereoImplGL alloc] initWithInterface:self];
+        if([CCSetup sharedSetup].graphicsAPI == CCGraphicsAPIMetal)
+        {
+            self.effectImpl = [[CCEffectStereoImplMetal alloc] initWithInterface:self];
+        }
+        else
+        {
+            self.effectImpl = [[CCEffectStereoImplGL alloc] initWithInterface:self];
+        }
+
         self.debugName = @"CCEffectStereo";
     }
     
